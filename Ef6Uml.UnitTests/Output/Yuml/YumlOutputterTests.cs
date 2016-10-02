9f089e31557826fdf0950e00dd4145f86c10c706
matthewrwilton <matthewrwilton@gmail.com>
2016-10-03 08:44:02 +1000

    Test refactoring.
    - Add ClassBuilder to simplify test setup and prevent having to rewrite tests as changes are made.

--- Ef6Uml.UnitTests/Output/Yuml/YumlOutputterTests.cs
+++ Ef6Uml.UnitTests/Output/Yuml/YumlOutputterTests.cs
@@ -12,9 +12,10 @@ namespace Ef6Uml.UnitTests.Output.Yuml
         public void Single_Class()
         {
             var target = new YumlOutputter();
-            var input = new Class("User", new Association[0]);
+            var user = new ClassBuilder()
+                .WithName("User");
 
-            var actual = target.Output(input);
+            var actual = target.Output(user);
             var expected = "[User]";
 
             actual.Should().Be(expected);
@@ -24,18 +25,44 @@ namespace Ef6Uml.UnitTests.Output.Yuml
         public void Simple_Association()
         {
             var target = new YumlOutputter();
-            var billingAddress = new Class("Billing Address", new Association[0]);
-            var customer = new Class(
-                "Customer", 
-                new List<Association>
-                {
-                    new Association(billingAddress)
-                });
+            var billingAddress = new ClassBuilder()
+                .WithName("Billing Address");
+            var customer = new ClassBuilder()
+                .WithName("Customer")
+                .WithAssociationTo(billingAddress);
 
             var actual = target.Output(customer);
             var expected = "[Customer]->[Billing Address]";
 
             actual.Should().Be(expected);
+        }
+
+        private class ClassBuilder
+        {
+            private List<Association> _associations = new List<Association>();
+            private string _name = "";
+
+            public static implicit operator Class(ClassBuilder classBuilder)
+            {
+                return classBuilder.Build();
+            }
+
+            public Class Build()
+            {
+                return new Class(_name, _associations);
+            }
+
+            public ClassBuilder WithName(string name)
+            {
+                _name = name;
+                return this;
+            }
+
+            public ClassBuilder WithAssociationTo(Class to)
+            {
+                _associations.Add(new Association(to));
+                return this;
+            }
         }
     }
 }
